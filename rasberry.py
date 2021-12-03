f6895b22d8d7549100dbe58a7c98e9db0c9af46e
ern3509 <93037156+ern3509@users.noreply.github.com>
2021-12-03 13:28:16 +0100

    Add files via upload
    Raspberrypy  aktualisiert

--- rasberry.py
+++ rasberry.py
@@ -7,34 +7,35 @@ from std_msgs.msg import Int16
 from sensor_msgs.msg import NavSatFix                                      #imports the data type of the GPS-Sensor's topic / message
 from geometry_msgs.msg import Twist 
 #from test.msg import signals 
-
+#keine Bewegung = 0 vorwärts = 1 rückwärts = 2 links = 3 rechts = 4
 
 #status= signals()
 
 #mode = 0
 pub = rospy.Publisher('steuerung', Int16, queue_size = 10)                  #topic called "steuerung" has been created here. The queue is being held in case the publishing frequency is higher than the loop on the subscriber's side
 mode = Int16()
-
+mode = 0
 
 
 def fahrtrichtung(msg):
     global mode
 
     rospy.loginfo("Fahrtichtung fkt called")
-    if msg.angular.z > 0:               
-        #status.Fahrtrichtung.data = 1                                           #rechts
-        mode = 0
+    if msg.angular.z > 0:                           #rechts
+        mode = 4
         #pub.publish(mode) 
-    elif msg.angular.z < 0:
-        #status.Fahrtrichtung.data = 2                                           #links
-        mode = 1
+    elif msg.angular.z < 0:                          #links
+                                     
+        mode = 3
         #pub.publish(mode)
-    elif msg.linear.x < 0:
-        
-        #status.Fahrtrichtung.data = 3                                           #rückwärts
+    elif msg.linear.x < 0:                           #rückwärts
         mode = 2
+    elif msg.linear.x > 0:
+        mode = 1
+    elif msg.linear.x == 0 and msg.linear.y == 0 and msg.linear.z == 0 and msg.angular.x == 0 and msg.angular.y == 0 and msg.angular.z == 0:   #klappt noch nicht
+        mode = 0
     
-    rospy.loginfo(mode)
+rospy.loginfo(mode)
 
 def gps_signal(msg):          
     rospy.loginfo("gps_signal fkt called")                                            #Aufruffunktion des Publishers
@@ -45,7 +46,9 @@ def gps_signal(msg):
         if(mode == 0): mode = 10
         elif(mode == 1): mode = 11
         elif(mode == 2): mode = 12
-        pub.publish(mode)
+        elif(mode == 3): mode = 13
+        elif(mode == 4): mode = 14
+        #pub.publish(mode)
 
         #pub.publish(status)                                                #der Message wird gepublished
     else:
@@ -53,7 +56,9 @@ def gps_signal(msg):
         if(mode == 0): mode = 20
         elif(mode == 1): mode = 21
         elif(mode == 2): mode = 22
-        pub.publish(mode)                                                #der Message wird gepublished
+        elif(mode == 3): mode = 23
+        elif(mode == 4): mode = 24
+        #pub.publish(mode)                                                #der Message wird gepublished
    
 def main():                                                               #Aufruffunktion des Suscribers
     rospy.init_node('status_signal', anonymous=True)                        
@@ -66,6 +71,7 @@ def main():                                                               #Aufru
 
     while not rospy.is_shutdown():
         rate.sleep()
+        pub.publish(mode)
 main()
 
 
